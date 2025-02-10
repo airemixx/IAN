9118cb13d95bda98af20e76a7260a99598588f3c
elaine <air105307@gmail.com>
2025-02-10 16:54:39 +0800

    更新課程分類nav

--- client/app/courses/page.js
+++ client/app/courses/page.js
@@ -2,11 +2,13 @@
 
 import React, { useState, useEffect } from 'react'
 import BannerPage from './course-banner/page'
+import CourseCategoryPage from './course-category/page'
 
-export default function CoursesPage(props) {
+export default function CoursesPage() {
   return (
     <>
     <BannerPage/>
+    <CourseCategoryPage/>
     </>
   )
 }
