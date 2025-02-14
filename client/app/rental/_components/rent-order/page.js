c5cea4fdd913a922e5106d0538bb298adf5adc8b
PK <hanhan010609@gmail.com>
2025-02-14 16:40:18 +0800

    租借元件更新

--- client/app/rental/_components/rent-order/page.js
+++ client/app/rental/_components/rent-order/page.js
@@ -1,9 +1,18 @@
 // rent-order
 
-export default function RentOrder() {
+'use client';
+
+export default function RentOrder({ setSorting }) {
   return (
-    <div>
-      <section></section>
+    <div className="d-flex justify-content-end mb-3">
+      <select
+        className="form-select w-auto"
+        onChange={(e) => setSorting(e.target.value)}
+      >
+        <option value="">排序方式</option>
+        <option value="asc">依價錢低到高</option>
+        <option value="desc">依價錢高到低</option>
+      </select>
     </div>
-  )
+  );
 }
