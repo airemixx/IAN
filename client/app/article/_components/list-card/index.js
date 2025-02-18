39f79413c5077c1463dd8f5c47bdbd9a0560fe5e
Vincen326 <OxblackdesertxO@gmail.com>
2025-02-18 14:28:34 +0800

    ad連結至產品+loop-ad

--- client/app/article/_components/list-card/index.js
+++ client/app/article/_components/list-card/index.js
@@ -1,20 +1,35 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './index.module.scss'
 import Link from 'next/link'
 
 const ListCard = ({ article, onTagClick, searchTerm }) => {
-  const rawTags = article.tags ? article.tags.split(",").map(tag => tag.trim()) : []
+  const [imageLoaded, setImageLoaded] = useState(false) // 文章圖片
+  const [authorImageLoaded, setAuthorImageLoaded] = useState(false) // 作者圖片
+  const rawTags = article.tags ? article.tags.split(',').map((tag) => tag.trim()) : []
   const uniqueTags = Array.from(new Set(rawTags))
+
+  const handleImageLoaded = () => {
+    // 圖片載入完成後設定狀態
+    setImageLoaded(true)
+  }
+
+  const handleAuthorImageLoaded = () => {
+    // 作者圖片載入完成後設定狀態
+    setAuthorImageLoaded(true)
+  }
 
   return (
     <div className={`${styles['y-list-card-area']}`}>
       <div className={`card ${styles['y-card']}`}>
         <img
           src={article.image_path || '/images/article/social.jpg'}
-          className={`card-img-top ${styles['y-card-img-top-css']}`}
+          className={`card-img-top ${styles['y-card-img-top-css']} ${styles['fade-in']} ${
+            imageLoaded ? styles['loaded'] : ''
+          }`}
           alt={article.title}
+          onLoad={handleImageLoaded} // 設定 onLoad 事件
         />
         <div className={`px-0 card-body ${styles['y-card-body-css']}`}>
           <div
@@ -23,10 +38,7 @@ const ListCard = ({ article, onTagClick, searchTerm }) => {
             <p className="mb-0">{article.category_name || '未分類'}</p>
           </div>
           <div className={`mb-5 ${styles['y-list-card-content']}`}>
-            <Link
-              href={`/article/${article.id}`}
-              className="text-decoration-none"
-            >
+            <Link href={`/article/${article.id}`} className="text-decoration-none">
               <h5 className="card-title">{article.title}</h5>
               <p
                 className={`${styles['card-sub-title']} ${styles['one-line-ellipsis']}`}
@@ -46,9 +58,12 @@ const ListCard = ({ article, onTagClick, searchTerm }) => {
           <div className={styles['y-author-date']}>
             <p className="mb-0">
               <img
-                className={`mb-2 ${styles['y-user-list-profile']} rounded-pill me-2`}
+                className={`mb-2 ${styles['y-user-list-profile']} rounded-pill me-2 ${styles['fade-in']} ${
+                  authorImageLoaded ? styles['loaded'] : ''
+                }`}
                 src={article.authorImageUrl || '/images/article/user (1).jpg'}
                 alt={article.author}
+                onLoad={handleAuthorImageLoaded}
               />
               {article.user_id || '編輯部'}
             </p>
