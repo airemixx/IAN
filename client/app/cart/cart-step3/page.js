fd563e93b442db04698d3efc9f271a78153ef11f
jer22366 <jer22356@yahoo.com.tw>
2025-02-20 11:02:25 +0800

    新增ECPay(還未跟購物車連接)及購物車流程初步完成

--- client/app/cart/cart-step3/page.js
+++ client/app/cart/cart-step3/page.js
@@ -15,55 +15,57 @@ export default function cartPageThree() {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
   const cartItems = JSON.parse(localStorage.getItem("cartItems"))
-  
+
   const cartProduct = []
   const cartLession = []
   const cartRent = []
-  
+
   let test = cartItems
-  Object.values(test).map(v =>{
-    switch(v.type){
-      case 'product': 
+  Object.values(test).map(v => {
+    switch (v.type) {
+      case 'product':
         cartProduct.push(v);
         break;
-      case 'lession' :
+      case 'lession':
         cartLession.push(v);
         break;
-      case 'rent' :
+      case 'rent':
         cartRent.push(v);
         break;
     }
   })
   return (
     <div className="container j-bodyHeight">
-    <div className=""></div>
+      <div className=""></div>
       <div className="row d-flex justify-content-center pt-4">
         <div className="j-shoppingCartBox justify-content-center col-12 col-sm-10 col-md-10 col-lg-8 col-xl-6 col-xxl-6 mt-5">
           <div className="j-cartItemsBox d-none d-sm-block p-0 d-flex flex-grow-1 flex-column gap-3">
             <div className='mt-2 mb-sm-5 j-itemBox'>
-              <h3 className='j-cartTitle mb-0 ps-3 pt-2 pb-2'>相機</h3>
+              {cartProduct.length != 0 ? <h3 className="j-cartTitle mb-0 ps-3 pt-2 pb-2">相機</h3> : ''}
               {cartProduct.map((item, index) => (
-                <div 
-                className={`j-input-box d-flex align-items-center mb-3 ${index > 0 ? "j-nextBox" : "" }`}
-                key={index}>
+                <div
+                  className={`j-input-box d-flex align-items-center mb-3 ${index > 0 ? "j-nextBox" : ""}`}
+                  key={index}>
                   <CartItem key={index} id={index + 1} itemData={item} />
                 </div>
               ))}
             </div>
-            
-            <div className='mb-sm-5 j-itemBox'>
-              <h3 className='mb-1 ms-3 pt-2'>課程</h3>
+
+            <div className="mt-2 mb-5 j-itemBox">
+              {cartLession.length != 0 ? <h3 className="j-cartTitle mb-0 ps-3 pt-2 pb-2">課程</h3> : ''}
               {cartLession.map((lession, index) => (
-                <div key={index}>
+                <div key={index}
+                  className={`j-input-box d-flex align-items-center mb-3 ${index > 0 ? "j-nextBox" : ""}`}>
                   <LessonItem key={index} lessionitem={lession} />
                 </div>
               ))}
             </div>
 
-            <div className='j-itemBox'>
-              <h3 className='mb-1 ms-3 pt-2'>租借</h3>
+            <div className="mt-2 j-itemBox">
+              {cartRent.length != 0 ? <h3 className="j-cartTitle mb-0 ps-3 pt-2 pb-2">租借</h3> : ''}
               {cartRent.map((rental, index) => (
-                <div key={index}>
+                <div key={index}
+                  className={`j-input-box d-flex align-items-center mb-3 ${index > 0 ? "j-nextBox" : ""}`}>
                   <RentItem key={index} rentalitem={rental} />
                 </div>
               ))}
@@ -73,15 +75,15 @@ export default function cartPageThree() {
             <div className='mt-2 mb-5 j-itemBox'>
               <h3 className='mb-1 ms-3 pt-2'>相機</h3>
               {cartProduct.map((item, index) => (
-                <div key={index+1}>
+                <div key={index + 1}>
                   <CartItem key={index} id={index + 1} itemData={item} />
                 </div>
               ))}
-            </div> 
+            </div>
             <div className='mt-2 mb-5 j-itemBox'>
               <h3 className='mb-1 ms-3 pt-2'>課程</h3>
               {cartLession.map((lession, index) => (
-                <div key={index+1}>
+                <div key={index + 1}>
                   <LessonItem key={index} lessionitem={lession} />
                 </div>
               ))}
@@ -89,7 +91,7 @@ export default function cartPageThree() {
             <div className='mt-2 j-itemBox'>
               <h3 className='mb-1 ms-3 pt-2'>租借</h3>
               {cartRent.map((rental, index) => (
-                <div key={index+1}>
+                <div key={index + 1}>
                   <RentItem key={index} rentalitem={rental} />
                 </div>
               ))}
