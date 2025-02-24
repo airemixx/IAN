ae2f0aedcb1b4f2f3f8caac8b0759a982d07596f
elaine <air105307@gmail.com>
2025-02-24 16:44:08 +0800

    剩老師頭像上傳

--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ import ordersRouter from './routes/orders.js'
 import linePayRouter from './routes/line-pay-test-only.js'
 import courseCtUploadRouter from './routes/courses-ct-upload.js'
 import courseCvUploadRouter from './routes/courses-cv-upload.js'
+import teacherUploadRouter from './routes/teacher-upload.js'
 
 // 讀取 .env 設定
 dotenv.config()
@@ -56,6 +57,7 @@ app.use('/api/teachers', teachersRouter)
 app.use("/api/auth", authRouter);
 app.use("/api/course-ct-upload", courseCtUploadRouter);
 app.use("/api/course-cv-upload", courseCvUploadRouter);
+app.use("/api/teacher-upload", teacherUploadRouter);
 app.use("/uploads", express.static(path.join(process.cwd(), "/public/uploads")));
 
 app.use('/api/rental', rentalRouter)
