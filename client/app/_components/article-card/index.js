fe9913d85e24ccf8fee4b41592674b8f6e7b828b
Yuzi514620 <92674952+Yuzi514620@users.noreply.github.com>
2025-02-27 21:00:44 +0800

    首頁更新

--- client/app/_components/article-card/index.js
+++ client/app/_components/article-card/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import styles from './ArticleCard.module.scss'
-
+import Link from 'next/link'
 export default function ArticleCardIndex(props) {
   return (
     <div className={styles.newsCard}>
@@ -14,32 +14,32 @@ export default function ArticleCardIndex(props) {
         {/* 卡片陣列 */}
         {[
           {
-            img: "images/HomePage-images/news (1).jpg",
-            category: "產品情報",
-            title: "Sony α1 II 當代全能旗艦相機重磅登場",
-            date: "2024.11.20",
-            link: "#",
+            img: "https://images.api.hahow.in/images/62f608443ed5430006718b31",
+            category: "攝影技巧",
+            title: "攝影入門必學：3 種常見的「焦段」與「視角」的變化者",
+            date: "2024.11.16",
+            link: "article/2",
           },
           {
-            img: "images/HomePage-images/news (1).webp",
+            img: "https://sw.cool3c.com/user/29442/2023/819804b5-5277-45e7-8e9e-5e056aee7c29.jpg?auto=compress&fix=max&w=2560",
+            category: "產品情報",
+            title: "徠卡推出第四代單色感光元件相機 Leica M11 Monochrom",
+            date: "2025.02.21",
+            link: "article/12",
+          },
+          {
+            img: "https://5mtz55f1.tinifycdn.com/storage/app/uploads/public/67a/456/38c/67a45638ce238730388063.jpg",
             category: "攝影活動",
-            title: "台北攝影器材展Nikon三大亮點，福袋加碼送Z系列鏡頭，總價值超過25萬",
-            date: "2024.12.31",
-            link: "#",
+            title: "年度最大攝影盛會！2025 SKM PHOTO攝影藝術博覽會，50組國內外名家探索影像之美",
+            date: "2024.12.16",
+            link: "article/5",
           },
           {
-            img: "images/HomePage-images/news (2).webp",
-            category: "產品情報",
-            title: "Leica Q3 43 評測報告｜APO 大光圈鏡頭加持、領略減法攝影的美好！",
-            date: "2024.10.11",
-            link: "#",
-          },
-          {
-            img: "images/HomePage-images/news (2).webp",
+            img: "https://images.hahow.in/images/636b2423e701e8c3e7013ffa",
             category: "訊息公告",
-            title: "2024 春節配送公告",
-            date: "2024.01.16",
-            link: "#",
+            title: "13 種拍照技巧輕鬆學，找出完美拍照角度，照片超好 PO！",
+            date: "2025.01.16",
+            link: "article/7",
           },
         ].map((news, index) => (
           <div className="col" key={index}>
@@ -66,7 +66,7 @@ export default function ArticleCardIndex(props) {
 
       {/* 查看更多按鈕 */}
       <div className={`text-center my-5 ${styles.btnMore}`}>
-        <button>查看更多</button>
+        <Link href="/article"><button>查看更多</button></Link>
       </div>
     </div>
   )
