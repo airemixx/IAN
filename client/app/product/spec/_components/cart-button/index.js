a6d884aa951c8e539b1c5b8efe0524dbddf197b1
jer22366 <jer22356@yahoo.com.tw>
2025-03-17 12:32:25 +0800

    add window.dispatchEvent

--- client/app/product/spec/_components/cart-button/index.js
+++ client/app/product/spec/_components/cart-button/index.js
@@ -36,12 +36,12 @@ export default function CartButton({ product }) {
     // 如果商品不在購物車中，則新增
     cart.push({ ...product, quantity: 1 })
     localStorage.setItem('cart', JSON.stringify(cart))
-
+    window.dispatchEvent(new Event('cartUpdated'))
     toast.success(`${product.name} 已成功加入購物車`, {
       position: 'top-right',
       autoClose: 1500,
       icon: <MdShoppingCart size={30} color="green" />,
-      className: styles.toastCustom,
+      className: styles.toastCustom,g
     })
   }
 
